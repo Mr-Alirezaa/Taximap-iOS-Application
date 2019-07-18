8a42bb4967b70affd54162275c1a621f38168ab6
Alireza Asadi <alireza.asadi.36@gmail.com>
2019-07-18 23:02:26 +0430

    SearchViewController added.

---
+++ Taximap/SearchViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SearchViewController.swift
+//  Taximap
+//
+//  Created by Alireza Asadi on 27/4/1398 AP.
+//  Copyright © 1398 AP Alireza Asadi. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
