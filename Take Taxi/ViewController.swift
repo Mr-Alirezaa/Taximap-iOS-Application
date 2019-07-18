e539d740405ae1e50172cb1ef3aa12420bad0e54
Alireza Asadi <alireza.asadi.36@gmail.com>
2019-07-18 20:58:25 +0430

    Initial Commit

---
+++ Take Taxi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Take Taxi
+//
+//  Created by Alireza Asadi on 27/4/1398 AP.
+//  Copyright © 1398 AP Alireza Asadi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
