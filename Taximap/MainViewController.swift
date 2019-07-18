b876fa476d5104e25288a88528606d22cad42db9
Alireza Asadi <alireza.asadi.36@gmail.com>
2019-07-19 00:22:05 +0430

    Project structure is ready.

--- Taximap/MainViewController.swift
+++ Taximap/MainViewController.swift
@@ -7,14 +7,87 @@
 //
 
 import UIKit
+import Mapbox
+import MapirServices
 
 class MainViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MGLMapView!
+
+    let routing = Routing.default
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        mapView.styleURL = MGLStyle.mapirVectorStyleURL
+    }
+
+    @IBAction func mapLongTapped(_ sender: UILongPressGestureRecognizer) {
+
     }
 
 
+    @IBAction func searchButtonTapped(_ sender: UIBarButtonItem) {
+        let searchViewController = self.storyboard?.instantiateViewController(withIdentifier: "search-view-controller") as! SearchViewController
+        searchViewController.delegate = self
+        searchViewController.centerCoordinate = mapView.centerCoordinate
+
+        self.present(searchViewController, animated: true, completion: nil)
+    }
+
+
+    @IBOutlet weak var routeButton: UIButton!
+    @IBOutlet weak var selectButton: UIButton!
+
+    private func updateView() {
+        if routing.canRoute {
+            routeButton.isEnabled = true
+        }
+        else {
+            routeButton.isEnabled = false
+        }
+
+        if selectedCoordinate == nil {
+            selectButton.isEnabled = false
+        } else {
+            selectButton.isEnabled = true
+        }
+    }
+
+    @IBAction func routeButtonTapped(_ sender: UIButton) {
+
+    }
+
+    var selectedCoordinate: CLLocationCoordinate2D? = nil
+
+    @IBAction func selectButtonTapped(_ sender: UIButton) {
+
+    }
+
+    @IBOutlet weak var detailsView: UIView!
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var durationLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+
+    @IBAction func cancelButtonTapped(_ sender: UIButton) {
+        detailsView.isHidden = true
+    }
 }
 
+extension MainViewController: MGLMapViewDelegate {
+
+}
+
+extension MainViewController: SearchViewControllerDelegate {
+    func searchViewController(selected: MPSSearchResult) {
+
+    }
+
+}
+
+extension MainViewController: RoutingDelegate {
+    func routing(_ routing: Routing, routeUpdated route: MPSRoute) {
+
+    }
+
+}
--- Taximap/SearchViewController.swift
+++ Taximap/SearchViewController.swift
@@ -7,11 +7,62 @@
 //
 
 import UIKit
+import MapirServices
+import CoreLocation
+
+protocol SearchViewControllerDelegate {
+    func searchViewController(selected: MPSSearchResult)
+}
 
 class SearchViewController: UIViewController {
 
+    var delegate: SearchViewControllerDelegate?
+
+    var centerCoordinate: CLLocationCoordinate2D?
+
+    @IBOutlet weak var searchResultsTableView: UITableView!
+
+    var searchResults: [MPSSearchResult] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        searchResultsTableView.delegate = self
+        searchResultsTableView.dataSource = self
+    }
+
+    @IBAction func searchButtonTapped(_ sender: UIButton) {
         
+    }
+
+}
+
+extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return searchResults.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "search-cell") {
+            cell.semanticContentAttribute = .forceRightToLeft
+            cell.detailTextLabel?.text = searchResults[indexPath.row].address
+            cell.textLabel?.text = searchResults[indexPath.row].title
+            return cell
+        }
+
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "search-cell")
+        cell.semanticContentAttribute = .forceRightToLeft
+        cell.detailTextLabel?.text = searchResults[indexPath.row].address
+        cell.textLabel?.text = searchResults[indexPath.row].title
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+
+        let selectedResult = searchResults[indexPath.row]
+        self.dismiss(animated: true) {
+            self.delegate?.searchViewController(selected: selectedResult)
+        }
     }
 }
